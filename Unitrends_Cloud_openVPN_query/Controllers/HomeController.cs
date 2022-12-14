fa109e59b1dcde3c62b5498f7df9c4f403d125d3
Raymond Lee Gregowicz <laserray777@lasercrops.com>
2022-12-13 19:54:57 -0600

    all project files included in this commit including all of wwwroot

---
+++ Unitrends_Cloud_openVPN_query/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Unitrends_Cloud_openVPN_query.Models;
+
+namespace Unitrends_Cloud_openVPN_query.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Unitrends_Cloud_openVPN_query/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Unitrends_Cloud_openVPN_query.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
